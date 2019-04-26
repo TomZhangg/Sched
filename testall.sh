5b3c1513eb58553c6f7740976247f81320e3cd17
Yoongbok Lee <js951204@naver.com>
2019-04-25 23:21:55 -0700

    forgot to add again

--- testall.sh
+++ testall.sh
@@ -11,7 +11,7 @@ make
 ./schedch.native -a tests/int_arithmetic_parsing/_smoke/intarith.sched > tests/int_arithmetic_parsing/_smoke/intarith.sched.out.tmp
 ./schedch.native -a tests/drop_statement_parsing/_smoke/drop.sched > tests/drop_statement_parsing/_smoke/drop.sched.out.tmp
 ./schedch.native -a tests/float_operations_parsing/_smoke/float.sched > tests/float_operations_parsing/_smoke/float.sched.out.tmp
-./schedch.native -a tests/if_else_parsing/_smoke/ifelse.sched > tests/if_else_parsing/_smoke/ifelse.sched.output 
+./schedch.native -a tests/if_else_parsing/_smoke/ifelse.sched > tests/if_else_parsing/_smoke/ifelse.sched.output
 
 ./schedch.native -s tests/print_sast.sched > tests/print_sast.sched.out.tmp
 ./schedch.native -c tests/print_sast.sched > tests/print_sast.ll && llc tests/print_sast.ll > tests/print_sast.s &&\
@@ -28,18 +28,6 @@ make
 ./schedch.native -l tests/float_op_semantics_codegen/_smoke/unop.sched > tests/float_op_semantics_codegen/_smoke/unop.sched.l_out.tmp
 ./schedch.native -l tests/int_op_semantics_codegen/_smoke/unop.sched > tests/int_op_semantics_codegen/_smoke/unop.sched.l_out.tmp
 
-<<<<<<< HEAD
-cmp --silent tests/create.sched.output tests/create.sched.out.tmp && echo '### SUCCESS: Files Are Identical! 1###' &&
-cmp --silent tests/insert_statement_parsing/_smoke/insert.sched.out.tmp tests/insert_statement_parsing/_smoke/insert.sched.out && echo '### SUCCESS: Files Are Identical! 2###' &&
-cmp --silent tests/set_statement_parsing/_smoke/set.sched.out.tmp tests/set_statement_parsing/_smoke/set.sched.out && echo '### SUCCESS: Files Are Identical! 3###' &&
-cmp --silent tests/func_definition/_smoke/func.sched.out.tmp tests/func_definition/_smoke/func.sched.out && echo '### SUCCESS: Files Are Identical! 4###' &&
-cmp --silent tests/boolean_operations_parsing/_smoke/boolean.sched.out.tmp tests/boolean_operations_parsing/_smoke/boolean.sched.output && echo '### SUCCESS: Files Are Identical! 5###' &&
-cmp --silent tests/print/_smoke/print.sched.out.tmp tests/print/_smoke/print.sched.output && echo '### SUCCESS: Files Are Identical! 6###' || echo '### WARNING: Files Are Different! ###'
-cmp --silent tests/print_sast.sched.out.tmp tests/print_sast.sched.out && echo '### SUCCESS: Files Are Identical! 7###' || echo '### WARNING: Files Are Different! ###'
-cmp --silent tests/print_sast.exe.out.tmp tests/print_sast.exe.out && echo '### SUCCESS: Files Are Identical! 8###' || echo '### WARNING: Files Are Different! ###'
-cmp --silent tests/bool_op_semantics\&codegen/_smoke/binop.sched.s_out.tmp tests/bool_op_semantics\&codegen/_smoke/binop.sched.s_out && echo '### SUCCESS: Files Are Identical! 9###' || echo '### WARNING: Files Are Different! ###'
-cmp --silent tests/bool_op_semantics\&codegen/_smoke/binop.sched.l_out.tmp tests/bool_op_semantics\&codegen/_smoke/binop.sched.l_out && echo '### SUCCESS: Files Are Identical! 10###' || echo '### WARNING: Files Are Different! ###'
-=======
 cmp --silent tests/create.sched.output tests/create.sched.out.tmp && echo '### SUCCESS: Files Are Identical! ###' &&
 cmp --silent tests/insert_statement_parsing/_smoke/insert.sched.out.tmp tests/insert_statement_parsing/_smoke/insert.sched.out && echo '### SUCCESS: Files Are Identical! ###' &&
 cmp --silent tests/set_statement_parsing/_smoke/set.sched.out.tmp tests/set_statement_parsing/_smoke/set.sched.out && echo '### SUCCESS: Files Are Identical! ###' &&
@@ -61,7 +49,6 @@ cmp --silent tests/float_op_semantics_codegen/_smoke/binop.sched.l_out.tmp tests
 cmp --silent tests/int_op_semantics_codegen/_smoke/unop.sched.l_out.tmp tests/int_op_semantics_codegen/_smoke/unop.sched.l_out && echo '### SUCCESS: Files Are Identical! ###' || echo '### WARNING: Files Are Different! ###'
 cmp --silent tests/float_op_semantics_codegen/_smoke/unop.sched.l_out.tmp tests/float_op_semantics_codegen/_smoke/unop.sched.l_out && echo '### SUCCESS: Files Are Identical! ###' || echo '### WARNING: Files Are Different! ###'
 cmp --silent tests/if_else_parsing/_smoke/ifelse.sched.out.tmp tests/if_else_parsing/_smoke/ifelse.sched.output && echo '### SUCCESS: Files Are Identical! ###' || echo '### WARNING: Files Are Different! ###'
->>>>>>> master
 
 
 make clean
