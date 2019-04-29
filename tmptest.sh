c8d589b96d295f77c4b12e3874ba0128c80be777
Yoongbok Lee <js951204@naver.com>
2019-04-29 06:52:15 -0700

    wow!

--- tmptest.sh
+++ tmptest.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 make clean
 make
 
-success = '### SUCCESS: Files Are Identical! ###'
-fail = '### WARNING: Files Are Different! ###'
+success='### SUCCESS: Files Are Identical! ###'
+fail='### WARNING: Files Are Different! ###'
 
 find . -name '*.sched' | while IFS= read -r line ; do
     echo "$line"
 		./schedch.native -a "$line" > "$line".aout.tmp
-		cmp --silent "$line".aout.tmp "$line".aout && echo $line $success || echo $line $fail
+		cmp --silent "$line".aout.tmp "$line".aout && echo "    $success" || echo "    $fail"
 		./schedch.native -s "$line" > "$line".sout.tmp
-		cmp --silent "$line".sout.tmp "$line".sout && echo $line $success || echo $line $fail
+		cmp --silent "$line".sout.tmp "$line".sout && echo "    $success" || echo "    $fail"
 		./schedch.native -l "$line" > "$line".lout.tmp
-		cmp --silent "$line".lout.tmp "$line".lout && echo $line $success || echo $line $fail
+		cmp --silent "$line".lout.tmp "$line".lout && echo "    $success" || echo "    $fail"
 done
